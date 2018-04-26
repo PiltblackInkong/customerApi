f77cc6ac3c1f0e9f07ab0ff708343ba4ef3e13a6
Chakhrit.S <chakhritz@gmail.com>
2018-04-27 01:58:18 +0700

    - initial commit

---
+++ src/main/java/com/chakhrit/controller/MainController.java
@@ -0,0 +1,61 @@
+package com.chakhrit.controller;
+
+import com.chakhrit.Custom.ResourceNotFoundException;
+import com.chakhrit.model.Customer;
+import com.chakhrit.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api")
+public class MainController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    //get all customers
+    @GetMapping("/customers")
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
+
+    //post customer
+    @PostMapping("/customers")
+    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    //get single customer
+    @GetMapping("/customers/{id}")
+    public Customer getCustomerById(@PathVariable(value = "id") Long id) {
+        return customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+    }
+
+    //update customer
+    @PutMapping("/customers/{id}")
+    public Customer updateCustomer(@PathVariable(value = "id") Long id, @Valid @RequestBody Customer customerDetail) {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+        customer.setEmail(customerDetail.getEmail());
+        customer.setPassword(customerDetail.getPassword());
+        customer.setName(customerDetail.getName());
+        customer.setLastname(customerDetail.getLastname());
+        customer.setAddress(customerDetail.getAddress());
+        customer.setTelno(customerDetail.getTelno());
+
+        Customer updateCustomer = customerRepository.save(customer);
+        return updateCustomer;
+    }
+
+    //delete xustomer
+    @DeleteMapping("/customers/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable(value = "id") Long id) {
+        Customer customer = customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+
+        customerRepository.delete(customer);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/chakhrit/model/Customer.java
@@ -0,0 +1,84 @@
+package com.chakhrit.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "customer")
+@EntityListeners(AuditingEntityListener.class)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String lastname;
+    @NotBlank
+    private String address;
+    @NotBlank
+    private String telno;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelno() {
+        return telno;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+}
