d41ed15af08cc1c6fb5e032529ce776c850198c4
Chakhrit.S <chakhritz@gmail.com>
2018-04-27 04:27:52 +0700

    no message

--- src/main/java/com/chakhrit/model/Customer.java
+++ src/main/java/com/chakhrit/model/Customer.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 public class Customer {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @NotBlank
