9f07a2d461543ff1c2ef0bb407f2f4d4d8b56670
PiltblackInkong <26704065+PiltblackInkong@users.noreply.github.com>
2018-05-08 19:14:05 +0700

    Update MainController.java

--- src/main/java/com/chakhrit/controller/MainController.java
+++ src/main/java/com/chakhrit/controller/MainController.java
@@ -15,14 +15,7 @@ import java.util.List;
 public class MainController {
     @Autowired
     private CustomerRepository customerRepository;
-    @OPTIONS
-      @Path("/getsample")
-      public Response getOptions() {
-        return Response.ok()
-          .header("Access-Control-Allow-Origin", "*")
-          .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
-          .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
-      }
+    
 
     //get all customers
     @GetMapping("/customers")
