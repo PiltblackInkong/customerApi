90d82d4fad19943dcc133d5e8ea9de66ccb6eb70
PiltblackInkong <26704065+PiltblackInkong@users.noreply.github.com>
2018-05-10 14:48:45 +0700

    Update MainController.java

--- src/main/java/com/chakhrit/controller/MainController.java
+++ src/main/java/com/chakhrit/controller/MainController.java
@@ -16,12 +16,7 @@ import java.util.List;
 
 @RequestMapping(path = "/api")
 
-  @CrossOrigin(origins = "http://localhost:9000")
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+ 
 
 public class MainController {
     @Autowired
