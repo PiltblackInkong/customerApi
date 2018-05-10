66d5e5d593b842d853d82c33df876bda472abb9d
PiltblackInkong <26704065+PiltblackInkong@users.noreply.github.com>
2018-05-11 02:58:14 +0700

    Update Application.java

--- src/main/java/com/chakhrit/Application.java
+++ src/main/java/com/chakhrit/Application.java
@@ -3,6 +3,8 @@ package com.chakhrit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableJpaAuditing
