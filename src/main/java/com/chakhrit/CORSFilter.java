4605949a9ef69da900e4156c4456cc7b06466f70
PiltblackInkong <26704065+PiltblackInkong@users.noreply.github.com>
2018-05-11 02:58:40 +0700

    Update CORSFilter.java

--- src/main/java/com/chakhrit/CORSFilter.java
+++ src/main/java/com/chakhrit/CORSFilter.java
@@ -4,26 +4,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * CORS Filter
+ *
+ * This filter is an implementation of W3C's CORS
+ * (Cross-Origin Resource Sharing) specification,
+ * which is a mechanism that enables cross-origin requests.
+ *
+ */
 public class CORSFilter extends GenericFilterBean implements Filter {
 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "*");
+//        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+
+        httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+//        httpResponse.setHeader("Access-Control-Allow-Headers",
+//                "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, X-Csrf-Token, Authorization");
+
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "false");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+
+        System.out.println("******** CORS Configuration Completed ********");
+
+        chain.doFilter(request, response);
+    }
 
 
-@Override
-public void doFilter(ServletRequest request, ServletResponse response,FilterChain chain)
-throws IOException,ServletExpection {
-    HttpServletResponse httpResponse = (HttpServletResponse) response;
-    httpResponse.setHander (name: "Access-Control-Allow-origin", value: "*");
-    httpResponse.setHander (name: "Access-Control-Allow-Methods", value:"*");
-    //httpResponse.setHander ("Access-Control-Allow-Methods","POST,GET,PUT,OPTIONs,DELETE");
-
-    httpResponse.setHander(name: "Access-Control-Allow-Headers", value: "*");
-   // httpResponse.setHander("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,X-Auth-Token,X-Csrf-Token,Authorization");
-
-httpResponse.setHander(name: "Access-Control-Allow-Credentials", value:"false");
-httpResponse.setHander(name: "Access-Control-Aloow-Max-Age", value:"3600");
-
-System.out.println("********** CORS configuration Completed **********");
-
-chain.doFilter(request, response);
-}
-}//The End
+} // The End
